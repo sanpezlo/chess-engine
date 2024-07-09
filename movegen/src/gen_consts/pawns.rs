7b4e6f41fbfbcffd221a7266bc5609d32ffbe693
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2024-07-09 15:01:26 -0500

    feat: add precomputed pawn moves

--- movegen/src/gen_consts/pawns.rs
+++ movegen/src/gen_consts/pawns.rs
@@ -1,4 +1,4 @@
-use chess_engine_core::{BitBoard, Color, Square};
+use chess_engine_core::{BitBoard, Color, Rank, Square};
 
 /// Returns the mask for the pawn attacks for a square.
 ///
@@ -50,9 +50,71 @@ pub const fn mask_pawn_attacks(color: Color, square: Square) -> BitBoard {
     attacks
 }
 
-/// Writes to a file the precomputed pawn attacks for all squares and colors.
+/// Returns the mask for the pawn moves for a square.
+///
+/// # Example
+///
+/// ```
+/// # use chess_engine_core::*;
+/// # use chess_engine_movegen::*;
+/// let white_pawn_attacks = mask_pawn_attacks(Color::White, Square::E4);
+/// let black_pawn_attacks = mask_pawn_attacks(Color::Black, Square::E4);
+///
+/// assert_eq!(white_pawn_attacks, bitboard!{
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . X . X . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+/// });
+/// assert_eq!(black_pawn_attacks, bitboard!{
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . X . X . .
+///     . . . . . . . .
+///     . . . . . . . .
+/// });
+/// ```
+pub const fn mask_pawn_moves(color: Color, square: Square) -> BitBoard {
+    let rank = square.rank();
+    let square = BitBoard(1u64 << square as usize);
+
+    let mut attacks = BitBoard::EMPTY;
+
+    match color {
+        Color::White => {
+            attacks = attacks.set_bit(square.up().0);
+
+            if rank as usize == Rank::Two as usize {
+                attacks = attacks.set_bit(square.up().up().0);
+            }
+        }
+        Color::Black => {
+            attacks = attacks.set_bit(square.down().0);
+
+            if rank as usize == Rank::Seven as usize {
+                attacks = attacks.set_bit(square.down().down().0);
+            }
+        }
+    }
+
+    attacks
+}
+
+/// Writes to a file
+///
+/// - the precomputed pawn attacks for all squares and colors.
+/// - the precomputed pawn moves for all squares and colors.
 pub fn write(f: &mut std::fs::File) {
     use std::io::Write;
+
+    // pawn attacks
 
     write!(f, "/// Precomputed pawn attacks for all squares\n").unwrap();
 
@@ -69,6 +131,29 @@ pub fn write(f: &mut std::fs::File) {
 
         for square in Square::ALL {
             write!(f, "\t\tBitBoard({}),\n", mask_pawn_attacks(color, square).0).unwrap();
+        }
+
+        write!(f, "\t],\n").unwrap();
+    }
+    write!(f, "];\n").unwrap();
+
+    // pawn moves
+
+    write!(f, "/// Precomputed pawn moves for all squares\n").unwrap();
+
+    write!(
+        f,
+        "pub const PAWN_MOVES: [[BitBoard; {}]; {}] = [\n",
+        Square::LEN,
+        Color::LEN
+    )
+    .unwrap();
+
+    for color in Color::ALL {
+        write!(f, "\t[\n").unwrap();
+
+        for square in Square::ALL {
+            write!(f, "\t\tBitBoard({}),\n", mask_pawn_moves(color, square).0).unwrap();
         }
 
         write!(f, "\t],\n").unwrap();
--- movegen/src/magic.rs
+++ movegen/src/magic.rs
@@ -1,6 +1,124 @@
-use chess_engine_core::{BitBoard, SlidingPiece, Square};
+use chess_engine_core::{BitBoard, Color, SlidingPiece, Square};
 
 include!(concat!(env!("OUT_DIR"), "/magic_gen.rs"));
+
+/// Returns the pawn attacks for a square and color.
+///
+/// # Example
+///
+/// ```
+/// # use chess_engine_core::*;
+/// # use chess_engine_movegen::*;
+/// let white_pawn_attacks = get_pawn_attacks(Color::White, Square::E4);
+/// let black_pawn_attacks = get_pawn_attacks(Color::Black, Square::E4);
+///
+/// assert_eq!(white_pawn_attacks, bitboard!{
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . X . X . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+/// });
+/// assert_eq!(black_pawn_attacks, bitboard!{
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . X . X . .
+///     . . . . . . . .
+///     . . . . . . . .
+/// });
+/// ```
+pub fn get_pawn_attacks(color: Color, square: Square) -> BitBoard {
+    PAWN_ATTACKS[color as usize][square as usize]
+}
+
+/// Returns the pawn moves for a square and color.
+///
+/// # Example
+///
+/// ```
+/// # use chess_engine_core::*;
+/// # use chess_engine_movegen::*;
+/// let white_pawn_attacks = get_pawn_moves(Color::White, Square::E2);
+/// let black_pawn_attacks = get_pawn_moves(Color::Black, Square::E7);
+///
+/// assert_eq!(white_pawn_attacks, bitboard!{
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . X . . .
+///     . . . . X . . .
+///     . . . . . . . .
+///     . . . . . . . .
+/// });
+/// assert_eq!(black_pawn_attacks, bitboard!{
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . X . . .
+///     . . . . X . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+/// });
+/// ```
+pub fn get_pawn_moves(color: Color, square: Square) -> BitBoard {
+    PAWN_MOVES[color as usize][square as usize]
+}
+
+/// Returns the king attacks for a square.
+///
+/// /// # Example
+///
+/// ```
+/// # use chess_engine_movegen::*;
+/// # use chess_engine_core::*;
+/// let king_attacks = get_king_attacks(Square::E4);
+///
+/// assert_eq!(king_attacks, bitboard!{
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . X X X . .
+///     . . . X . X . .
+///     . . . X X X . .
+///     . . . . . . . .
+///     . . . . . . . .
+/// });
+/// ```
+pub fn get_king_attacks(square: Square) -> BitBoard {
+    KING_ATTACKS[square as usize]
+}
+
+/// Returns the knight attacks for a square.
+///
+/// # Example
+///
+/// ```
+/// # use chess_engine_movegen::*;
+/// # use chess_engine_core::*;
+/// let knight_attacks = get_knight_attacks(Square::E4);
+///
+/// assert_eq!(knight_attacks, bitboard!{
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . X . X . .
+///     . . X . . . X .
+///     . . . . . . . .
+///     . . X . . . X .
+///     . . . X . X . .
+///     . . . . . . . .
+/// });
+/// ```
+pub fn get_knight_attacks(square: Square) -> BitBoard {
+    KNIGHT_ATTACKS[square as usize]
+}
 
 /// Returns the bishop attacks for a square with blockers.
 ///
@@ -33,13 +151,6 @@ include!(concat!(env!("OUT_DIR"), "/magic_gen.rs"));
 /// ```
 pub fn get_bishop_attacks(square: Square, blockers: BitBoard) -> BitBoard {
     let magic_index = blockers.0 & RELEVANT_BISHOP_BLOCKERS[square as usize].0;
-    println!("{:?}", BitBoard(magic_index));
-    println!("{:?}", RELEVANT_BISHOP_BLOCKERS[square as usize]);
-    println!(
-        "{:?}",
-        mask_blockers(0b1001011, RELEVANT_BISHOP_BLOCKERS[square as usize])
-    );
-    println!("{:?}", BISHOP_ATTACKS[square as usize][0b1001011 as usize]);
     let magic_index = magic_index
         .wrapping_mul(MAGIC_NUMBERS[SlidingPiece::Bishop as usize][square as usize])
         >> (64 - RELEVANT_BISHOP_BLOCKERS_COUNT[square as usize]);
@@ -78,13 +189,6 @@ pub fn get_bishop_attacks(square: Square, blockers: BitBoard) -> BitBoard {
 /// ```
 pub fn get_rook_attacks(square: Square, blockers: BitBoard) -> BitBoard {
     let magic_index = blockers.0 & RELEVANT_ROOK_BLOCKERS[square as usize].0;
-    println!("{:?}", BitBoard(magic_index));
-    println!("{:?}", RELEVANT_ROOK_BLOCKERS[square as usize]);
-    println!(
-        "{:?}",
-        mask_blockers(0b1001011, RELEVANT_ROOK_BLOCKERS[square as usize])
-    );
-    println!("{:?}", ROOK_ATTACKS[square as usize][0b1001011 as usize]);
     let magic_index = magic_index
         .wrapping_mul(MAGIC_NUMBERS[SlidingPiece::Rook as usize][square as usize])
         >> (64 - RELEVANT_ROOK_BLOCKERS_COUNT[square as usize]);
@@ -92,21 +196,35 @@ pub fn get_rook_attacks(square: Square, blockers: BitBoard) -> BitBoard {
     ROOK_ATTACKS[square as usize][magic_index as usize]
 }
 
-const fn mask_blockers(pattern: u64, blockers: BitBoard) -> BitBoard {
-    let mut mask = BitBoard::EMPTY;
-    let mut blockers = blockers;
-
-    let mut i = 0;
-
-    while let Some(square) = blockers.least_significant_square() {
-        blockers = blockers.unset_square(square);
-
-        if pattern & (1 << i) != 0 {
-            mask = mask.set_square(square);
-        }
-
-        i += 1;
-    }
-
-    mask
+/// Returns the queen attacks for a square with blockers.
+///
+/// # Example
+///
+/// ```
+/// # use chess_engine_movegen::*;
+/// # use chess_engine_core::*;
+/// let attacks = get_queen_attacks(Square::E4, bitboard!{
+///     . . . . . . . .
+///     . X . . X . . .
+///     . . . . . . X .
+///     . . . X X . . .
+///     . X . . . . . X
+///     . . . . . . . .
+///     . . X . X . . .
+///     . . . . X . . X
+/// });
+///
+/// assert_eq!(attacks, bitboard!{
+///     . . . . . . . .
+///     . . . . . . . .
+///     . . . . . . X .
+///     . . . X X X . .
+///     . X X X . X X X
+///     . . . X X X . .
+///     . . X . X . X .
+///     . . . . . . . X
+/// });
+/// ```
+pub fn get_queen_attacks(square: Square, blockers: BitBoard) -> BitBoard {
+    get_bishop_attacks(square, blockers) | get_rook_attacks(square, blockers)
 }
